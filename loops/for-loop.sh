4d9b50ab7f111d8ab035192932afa5393a16d96b
Ezra <ezraa619@gmail.com>
2022-01-14 10:59:30 -0800

    Write for loop program

--- loops/for-loop.sh
+++ loops/for-loop.sh
@@ -1,1 +1,11 @@
-#something
+#for-loop examples
+
+cards = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10)
+
+echo "how many cards would you like?"
+echo "input a number 1 - 10"
+read value
+
+for i in $(seq 1 $value);
+do echo${cards[RANDOM%${#cards[@]}]);
+done
